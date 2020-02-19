d191596c6dab56ce5a15ff96dde75072f1280c16
Andre Luis Miranda <almiranda86@gmail.com>
2020-02-19 17:09:02 -0300

    HashTables

---
+++ AlgorithmsCSharp/Algorithms/HashTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsCSharp
+{
+    public class HashTable
+    {
+        private Dictionary<dynamic, dynamic> items;
+
+        public HashTable()
+        {
+            items = new Dictionary<dynamic, dynamic>();
+        }
+
+        public void populateDictionary(dynamic key, dynamic value)
+        {
+            items.Add(key, value);
+        }
+
+        public void replaceDictionaryItem(dynamic key, dynamic value)
+        {
+            items[key] = value;
+        }
+
+        public string printDictionary()
+        {
+            string dictionary = $"";
+
+            foreach(var item in items)
+            {
+                dictionary += $"Chave: {item.Key } Valor: {item.Value} ";
+            }
+
+            return dictionary.Trim();
+        }
+
+        public int dictionaryCount()
+        {
+            return items.Count;
+        }
+    }
+}
---
+++ AlgorithmsCSharp/AlgoritmTest/HashTableTest.cs
@@ -0,0 +1,42 @@
+﻿using AlgorithmsCSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoritmTest
+{
+    [TestClass]
+    public class HashTableTest
+    {
+        private HashTable InsertItem()
+        {
+            HashTable hashTable = new HashTable();
+            hashTable.populateDictionary(1, "A");
+            hashTable.populateDictionary(2, "B");
+            hashTable.populateDictionary(3, "C");
+            hashTable.populateDictionary(4, "D");
+            return hashTable;
+        }
+
+
+        [TestMethod]
+        public void dictionaryCountTest()
+        {
+            HashTable hashTable = InsertItem();
+            int count = hashTable.dictionaryCount();
+
+            Assert.AreEqual(4, count);
+        }
+
+        [TestMethod]
+        public void dictionaryPrintTest()
+        {
+            HashTable hashTable = InsertItem();
+            string expected = "Chave: 1 Valor: A Chave: 2 Valor: B Chave: 3 Valor: C Chave: 4 Valor: D";
+            string result = hashTable.printDictionary();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
